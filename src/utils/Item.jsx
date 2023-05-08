1fd6ef24e42f44797cf70fe97d899a6f3cef971f
Mutlu Gülerce <118522613+mutlugulerce@users.noreply.github.com>
2023-05-08 05:12:44 +0300

    salesjsx tamamlama

--- src/utils/Item.jsx
+++ src/utils/Item.jsx
@@ -1,11 +1,11 @@
 import { ShoppingBagIcon, StarIcon } from "@heroicons/react/24/solid";
 import React from "react";
 
-const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
+const Item = ({ ifExists,id, color, shadow, title, text, img, btn, rating, price }) => {
   return (
     <>
-      <div className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center`} >
-        <div className="grid items-center justify-items-center rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105">
+      <div className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center  ${ifExists ? 'justify-items-start' : 'justify-items-center'} rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`} >
+        <div className={`grid items-center justify-items-center ${ifExists ? 'justify-items-start' : 'justify-items-center'}`}>
           <h1 className="text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow ">{title} </h1>
           <p className="text-slate-200 filter drop-shadow text-base md:text-sm font-normal ">{text} </p>
           <div className="flex items-center justify-between w-28 my-2">
@@ -22,9 +22,9 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
             <button type="button" className="bg-white/90 blue-effect-theme button-theme p-0.5 shadow shadow-sky-200 text-sm text-black">{btn}</button>
           </div>
         </div>
-        <div className="flex items-center">
-          <img src={img} alt="img/item-img" 
-          className="h-36 w-64 transitions-theme hover:-rotate-12"
+        <div className={`flex items-center ${ifExists? 'absolute top-5 right-1' : 'justify-center'}`}>
+          <img src={img} alt={`img/item-img ${id}`}
+          className={`h-36 w-64 transitions-theme hover:-rotate-12 ${ifExists? 'h-auto w-64 lg:w-56 md:w-48 -rotate-[35deg]' : 'h-36 w-64'}`}
           />
         </div>
       </div>
