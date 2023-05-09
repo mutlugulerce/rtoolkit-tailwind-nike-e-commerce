a3528dca50cc5c5860696dff044a17dbbcee4d02
Mutlu Gülerce <118522613+mutlugulerce@users.noreply.github.com>
2023-05-09 16:36:40 +0300

    navbar inşa ve import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { FlexContent, Footer, Hero, Sales, Stories } from "./components";
+import { FlexContent, Footer, Hero, Navbar, Sales, Stories } from "./components";
 import { heroapi, popularsales, toprateslaes, highlight,sneaker, story,footerAPI} from "./data/data.js";
 
 const App = () => {
   return (
     <>
     <main className="flex flex-col gap-16 relative">
+      <Navbar/>
       <Hero heroapi={heroapi} />
       <Sales endpoint={popularsales} ifExists/>
       <FlexContent endpoint={highlight}  ifExists/>
